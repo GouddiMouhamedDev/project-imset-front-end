339ea107ef50e06e35f29702c61e001add813db5
GouddiMouhamedDev <toonmouhamed@gmail.com>
2024-08-24 01:23:46 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-      },  
-    
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
+  // Ajouter la configuration pour le export statique
+  output: 'export',
+  // Autres configurations éventuelles
 }
 
 module.exports = nextConfig
